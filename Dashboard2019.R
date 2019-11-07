9099dc44248b5a907279d61984aead90767365f3
David Dobrowski <dobrowski@gmail.com>
2019-11-07 15:55:08 -0800

    Fix DA calculation with the multiple Priority 4 ways

--- Dashboard2019.R
+++ Dashboard2019.R
@@ -119,26 +119,37 @@ all <- all %>%
 
 mry.DA <- all %>%
     filter(str_detect(countyname, "Monterey"), # Change County Name as Applicable
-           year == "2019",
+           year == "2018",
   #          color == "1",
-           ( rtype == "D"|  charter_flag == "Y")
+           rtype == "D" 
+  #        ( rtype == "D"|  charter_flag == "Y")
     ) %>%
     select(cds, schoolname, districtname, charter_flag, studentgroup, ind, color, year) %>%
     group_by(cds, studentgroup) %>%
     mutate(Priority5 = if_else( (ind == "chronic" & color == 1)|(ind == "grad" & color == 1), TRUE, FALSE ),
            Priority6 = if_else( (ind == "susp" & color == 1), TRUE, FALSE ),
            Priority8 = if_else( (ind == "cci" & color == 1), TRUE, FALSE ),
-            Priority4math = case_when( ind == "math" & color == 1  ~ 1, 
+            Prior4math = case_when( ind == "math" & color == 1  ~ 1, 
                                        ind == "math" & color == 2  ~ .5, 
                                      TRUE ~ 0),
-           Priority4ela = case_when( ind == "ela" & color == 1  ~ 1, 
+           Prior4ela = case_when( ind == "ela" & color == 1  ~ 1, 
                                       ind == "ela" & color == 2  ~ .5, 
                                       TRUE ~ 0),
-           Priority4elpi = case_when( ind == "elpi" & color == 1  ~ 1.5, 
+           Prior4elpi = case_when( ind == "elpi" & color == 1  ~ 1.5, # Will need to see if this is coded as color or only "Very low" 
                                       TRUE ~ 0),
-           Priority4 = if_else(Priority4ela + Priority4math + Priority4elpi > 1, TRUE, FALSE )
+      #     Priority4 = if_else(Priority4ela + Priority4math + Priority4elpi > 1, TRUE, FALSE )
            ) %>%
-    mutate(DA.count = Priority4 + Priority5 + Priority6 + Priority8   )
+    mutate(Priority4.count = sum(Prior4ela + Prior4math + Prior4elpi),
+           Priority4 = if_else(Priority4.count > 1, TRUE, FALSE )) %>%
+    mutate(Priority5 = as.logical(sum(Priority5)),
+           Priority6 = as.logical(sum(Priority6)),
+           Priority8 = as.logical(sum(Priority8))
+           ) %>%
+    select(cds, districtname ,studentgroup, Priority4 , Priority5 , Priority6 , Priority8) %>%
+    distinct() %>%
+    mutate(DA.count =  Priority4 + Priority5 + Priority6 + Priority8 ) %>%
+    filter(DA.count >= 2) %>%
+    arrange(districtname, studentgroup)
 
 
 #### Graphing -------
@@ -149,7 +160,8 @@ yrs <-2018
 graphthis <- all %>%
     filter(str_detect(countyname, "Monterey"), 
            year == yrs,
-            charter_flag == "Y"
+#            charter_flag == "Y"
+           rtype == "D"
 #           ( rtype == "D"|  charter_flag == "Y")
            ) %>%
     mutate(combo.name = paste0(districtname," - ",schoolname),
